81ca3c76ce51f6e9102679391938d79a17eab90b
sergey20240124 <157232852+sergey20240124@users.noreply.github.com>
2026-05-09 00:08:32 +0300

    Add files via upload

---
+++ leagues_snippet.js
@@ -0,0 +1,7 @@
+const LEAGUES = [
+  { id: 'EPL',        label: 'EPL' },
+  { id: 'La Liga',    label: 'La Liga' },
+  { id: 'Serie A',    label: 'Serie A' },
+  { id: 'Bundesliga', label: 'Bundesliga' },
+  { id: 'Ligue 1',    label: 'Ligue 1' },
+];
